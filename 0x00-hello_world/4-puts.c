9572980f50ae5a7898f0c36e66151f0baeb4fa20
Kingsley Ogochukwu <kingsleyh4real6294@gmail.com>
2023-03-20 17:45:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * Return: 0
+ * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like a building in a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
