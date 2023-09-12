7f4bb03799be75499926b33e2dd9df7c8324de99
kinzy-louis <kingsley4real6294@gmail.com>
2023-09-12 05:37:50 -0700

    Hello wolrd commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
+}
