df550e8f646ad4e68d5e13bc1a1b308c1dfbb938
kinzy-louis <kingsley4real6294@gmail.com>
2023-09-12 04:20:14 -0700

    Hello wolrd commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - entry point
+ * main - A program that prints wit puts function
  *
- * Return - Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
